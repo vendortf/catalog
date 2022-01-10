9a1ab9333959c4cc66a2ecc77e8c81c751fd70f7
HilliamT <hilliamtung@gmail.com>
2022-01-10 10:41:39 +0000

    :pencil2: Exported stripFullSKU

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
-import { EconItem } from "tf2-item-format";
-import { toBaseSKU } from "./lib/toBaseSKU";
-import { toFullSKU } from "./lib/toFullSKU";
-import { parseSKU } from "./lib/parseSKU";  
-import { toBaseName } from "./lib/toName";
+import { EconItem } from "tf2-item-format"
+import { toBaseSKU } from "./lib/toBaseSKU"
+import { toFullSKU } from "./lib/toFullSKU"
+import { parseSKU } from "./lib/parseSKU"
+import { toBaseName } from "./lib/toName"
+import { stripFullSKU } from "./lib/stripFullSKU"
 
-export {
-	EconItem,
-	toBaseSKU,
-	toFullSKU,
-	parseSKU,
-	toBaseName
-};
+export { EconItem, toBaseSKU, toFullSKU, parseSKU, toBaseName, stripFullSKU }
