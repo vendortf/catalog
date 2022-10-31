211aca8629a170a4c6c2d0a54030d459b6cfc23c
piman51277 <piman.dev@gmail.com>
2022-10-31 11:22:59 -0400

    fix indents

--- lib/enums/Spell.ts
+++ lib/enums/Spell.ts
@@ -1,65 +1,66 @@
 export enum Spell {
-  PutrescentPigmentation = 8900,
-  DieJob = 8901,
-  ChromaticCorruption = 8902,
-  SpectralSpectrum = 8903,
-  SinisterStaining = 8904,
-  VoicesFromBelow = 8905,
-  TeamSpiritFootprints = 8914,
-  GangreenFootprints = 8915,
-  CorpseGrayFootprints = 8916,
-  ViolentVioletFootprints = 8917,
-  RottenOrangeFootprints = 8918,
-  BruisedPurpleFootprints = 8919,
-  HeadlessHorseshoes = 8920,
-  Exorcism = 8921,
-  PumpkinBombs = 8922,
-  HalloweenFire = 8923,
+	PutrescentPigmentation = 8900,
+	DieJob = 8901,
+	ChromaticCorruption = 8902,
+	SpectralSpectrum = 8903,
+	SinisterStaining = 8904,
+	VoicesFromBelow = 8905,
+	TeamSpiritFootprints = 8914,
+	GangreenFootprints = 8915,
+	CorpseGrayFootprints = 8916,
+	ViolentVioletFootprints = 8917,
+	RottenOrangeFootprints = 8918,
+	BruisedPurpleFootprints = 8919,
+	HeadlessHorseshoes = 8920,
+	Exorcism = 8921,
+	PumpkinBombs = 8922,
+	HalloweenFire = 8923,
 }
 
 const SpellEnums = {
-  "Putrescent Pigmentation": Spell.PutrescentPigmentation,
-  "Die Job": Spell.DieJob,
-  "Chromatic Corruption": Spell.ChromaticCorruption,
-  "Spectral Spectrum": Spell.SpectralSpectrum,
-  "Sinister Staining": Spell.SinisterStaining,
-  "Voices From Below": Spell.VoicesFromBelow,
-  "Team Spirit Footprints": Spell.TeamSpiritFootprints,
-  "Gangreen Footprints": Spell.GangreenFootprints,
-  "Corpse Gray Footprints": Spell.CorpseGrayFootprints,
-  "Violent Violet Footprints": Spell.ViolentVioletFootprints,
-  "Rotten Orange Footprints": Spell.RottenOrangeFootprints,
-  "Bruised Purple Footprints": Spell.BruisedPurpleFootprints,
-  "Headless Horseshoes": Spell.HeadlessHorseshoes,
-  Exorcism: Spell.Exorcism,
-  "Pumpkin Bombs": Spell.PumpkinBombs,
-  "Halloween Fire": Spell.HalloweenFire,
+	"Putrescent Pigmentation": Spell.PutrescentPigmentation,
+	"Die Job": Spell.DieJob,
+	"Chromatic Corruption": Spell.ChromaticCorruption,
+	"Spectral Spectrum": Spell.SpectralSpectrum,
+	"Sinister Staining": Spell.SinisterStaining,
+	"Voices From Below": Spell.VoicesFromBelow,
+	"Team Spirit Footprints": Spell.TeamSpiritFootprints,
+	"Gangreen Footprints": Spell.GangreenFootprints,
+	"Corpse Gray Footprints": Spell.CorpseGrayFootprints,
+	"Violent Violet Footprints": Spell.ViolentVioletFootprints,
+	"Rotten Orange Footprints": Spell.RottenOrangeFootprints,
+	"Bruised Purple Footprints": Spell.BruisedPurpleFootprints,
+	"Headless Horseshoes": Spell.HeadlessHorseshoes,
+	Exorcism: Spell.Exorcism,
+	"Pumpkin Bombs": Spell.PumpkinBombs,
+	"Halloween Fire": Spell.HalloweenFire,
 
-  [Spell.PutrescentPigmentation]: "Putrescent Pigmentation",
-  [Spell.DieJob]: "Die Job",
-  [Spell.ChromaticCorruption]: "Chromatic Corruption",
-  [Spell.SpectralSpectrum]: "Spectral Spectrum",
-  [Spell.SinisterStaining]: "Sinister Staining",
-  [Spell.VoicesFromBelow]: "Voices From Below",
-  [Spell.TeamSpiritFootprints]: "Team Spirit Footprints",
-  [Spell.GangreenFootprints]: "Gangreen Footprints",
-  [Spell.CorpseGrayFootprints]: "Corpse Gray Footprints",
-  [Spell.ViolentVioletFootprints]: "Violent Violet Footprints",
-  [Spell.RottenOrangeFootprints]: "Rotten Orange Footprints",
-  [Spell.BruisedPurpleFootprints]: "Bruised Purple Footprints",
-  [Spell.HeadlessHorseshoes]: "Headless Horseshoes",
-  [Spell.Exorcism]: "Exorcism",
-  [Spell.PumpkinBombs]: "Pumpkin Bombs",
-  [Spell.HalloweenFire]: "Halloween Fire",
-}
+	[Spell.PutrescentPigmentation]: "Putrescent Pigmentation",
+	[Spell.DieJob]: "Die Job",
+	[Spell.ChromaticCorruption]: "Chromatic Corruption",
+	[Spell.SpectralSpectrum]: "Spectral Spectrum",
+	[Spell.SinisterStaining]: "Sinister Staining",
+	[Spell.VoicesFromBelow]: "Voices From Below",
+	[Spell.TeamSpiritFootprints]: "Team Spirit Footprints",
+	[Spell.GangreenFootprints]: "Gangreen Footprints",
+	[Spell.CorpseGrayFootprints]: "Corpse Gray Footprints",
+	[Spell.ViolentVioletFootprints]: "Violent Violet Footprints",
+	[Spell.RottenOrangeFootprints]: "Rotten Orange Footprints",
+	[Spell.BruisedPurpleFootprints]: "Bruised Purple Footprints",
+	[Spell.HeadlessHorseshoes]: "Headless Horseshoes",
+	[Spell.Exorcism]: "Exorcism",
+	[Spell.PumpkinBombs]: "Pumpkin Bombs",
+	[Spell.HalloweenFire]: "Halloween Fire",
+};
 
 export function getSpellEnum(spell: string): Spell {
-  const spellName = spell.replace("Halloween Spell: ", "")
-  if (spellName in SpellEnums) return SpellEnums[spellName]
-  throw "Spell not found"
+	const spellName = spell.replace("Halloween Spell: ", "");
+	if (spellName in SpellEnums) return SpellEnums[spellName];
+	throw "Spell not found";
 }
 
 export function getSpellName(spellEnum: Spell): string {
-  if (spellEnum in SpellEnums) return "Halloween Spell: " + SpellEnums[spellEnum]
-  throw "SpellEnum not found"
+	if (spellEnum in SpellEnums)
+		return "Halloween Spell: " + SpellEnums[spellEnum];
+	throw "SpellEnum not found";
 }
--- lib/toFullSKU.ts
+++ lib/toFullSKU.ts
@@ -1,48 +1,55 @@
-import { EconItem, parseEconItem, schema } from "tf2-item-format"
-import { toBaseFromParsedEcon } from "./toBaseSKU"
-import { getSheenEnum } from "./enums/Sheen"
-import { getKillstreakerEnum } from "./enums/Killstreaker"
-import { SKUPrefix } from "./enums/SKUPrefix"
-import { getSpellEnum } from "./enums/Spell"
-import { getPartDefindex } from "./enums/Part"
+import { EconItem, parseEconItem, schema } from "tf2-item-format";
+import { toBaseFromParsedEcon } from "./toBaseSKU";
+import { getSheenEnum } from "./enums/Sheen";
+import { getKillstreakerEnum } from "./enums/Killstreaker";
+import { SKUPrefix } from "./enums/SKUPrefix";
+import { getSpellEnum } from "./enums/Spell";
+import { getPartDefindex } from "./enums/Part";
 
 /**
  * Return full SKU of EconItem
  */
 export function toFullSKU(econItem: EconItem): string {
-  const parsedEconItem = parseEconItem(econItem, true, true)
+	const parsedEconItem = parseEconItem(econItem, true, true);
 
-  const { paint, parts, spells, sheen, killstreaker, tradable } = parsedEconItem
-  let sku = toBaseFromParsedEcon(parsedEconItem, econItem)
+	const { paint, parts, spells, sheen, killstreaker, tradable } =
+		parsedEconItem;
+	let sku = toBaseFromParsedEcon(parsedEconItem, econItem);
 
-  function attachToSKU(component) {
-    sku = `${sku};${component}`
-  }
+	function attachToSKU(component) {
+		sku = `${sku};${component}`;
+	}
 
-  if (paint != null) attachToSKU(`${SKUPrefix.Paint}${schema.getDefindex(paint)}`)
+	if (paint != null)
+		attachToSKU(`${SKUPrefix.Paint}${schema.getDefindex(paint)}`);
 
-  if (parts != null) {
-    const partsDefindices = []
-    for (const part of parts) partsDefindices.push(getPartDefindex(`Strange Part: ${part}`))
-    partsDefindices.sort()
-    for (const partDefindex of partsDefindices) {
-      if (partDefindex == null) continue // skip default parts
-      attachToSKU(`${SKUPrefix.Part}${partDefindex}`)
-    }
-  }
+	if (parts != null) {
+		const partsDefindices = [];
+		for (const part of parts)
+			partsDefindices.push(getPartDefindex(`Strange Part: ${part}`));
+		partsDefindices.sort();
+		for (const partDefindex of partsDefindices) {
+			if (partDefindex == null) continue; // skip default parts
+			attachToSKU(`${SKUPrefix.Part}${partDefindex}`);
+		}
+	}
 
-  if (spells != null) {
-    const spellsDefindices = []
-    for (const spell of spells) spellsDefindices.push(getSpellEnum(`Halloween Spell: ${spell}`))
-    spellsDefindices.sort()
-    for (const spellDefindex of spellsDefindices) attachToSKU(`${SKUPrefix.Spell}${spellDefindex}`)
-  }
+	if (spells != null) {
+		const spellsDefindices = [];
+		for (const spell of spells)
+			spellsDefindices.push(getSpellEnum(`Halloween Spell: ${spell}`));
+		spellsDefindices.sort();
+		for (const spellDefindex of spellsDefindices)
+			attachToSKU(`${SKUPrefix.Spell}${spellDefindex}`);
+	}
 
-  if (sheen != null) attachToSKU(`${SKUPrefix.Sheen}${getSheenEnum(sheen)}`)
-  if (killstreaker != null)
-    attachToSKU(`${SKUPrefix.Killstreaker}${getKillstreakerEnum(killstreaker)}`)
+	if (sheen != null) attachToSKU(`${SKUPrefix.Sheen}${getSheenEnum(sheen)}`);
+	if (killstreaker != null)
+		attachToSKU(
+			`${SKUPrefix.Killstreaker}${getKillstreakerEnum(killstreaker)}`
+		);
 
-  if (!tradable) attachToSKU("untradable")
+	if (!tradable) attachToSKU("untradable");
 
-  return sku
+	return sku;
 }
